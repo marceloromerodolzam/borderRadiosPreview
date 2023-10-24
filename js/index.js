112692704814b94732f921850fd62589c1c6bb59
marceloromerodolzam <cucabolado2@gmail.com>
2023-10-24 12:30:10 -0300

    update comit

--- js/index.js
+++ js/index.js
@@ -4,11 +4,17 @@ const bottomLeft = document.getElementById('bottom-left');
 const bottomRight = document.getElementById('bottom-right');
 const box = document.getElementById('box');
 const btncopiar = document.getElementById('copiar');
+const widthInput = document.getElementById('width');
+const heightInput = document.getElementById('height');
 
 topLeft.addEventListener('input', updateBorderRadius);
 topRight.addEventListener('input', updateBorderRadius);
 bottomLeft.addEventListener('input', updateBorderRadius);
 bottomRight.addEventListener('input', updateBorderRadius);
+btncopiar.addEventListener('click', copyCSS);
+
+widthInput.addEventListener('input', updateWidthValue);
+heightInput.addEventListener('input', updateHeightValue);
 
 function updateBorderRadius(){
 
@@ -22,11 +28,27 @@ function updateBorderRadius(){
     
 }
 
-btncopiar.addEventListener('click', copyCSS);
+function updateWidthValue(){
+
+    const Wdth = widthInput.value + 'px';
+    const Hght = heightInput.value + 'px';
+
+    const widthHeightValue = `${Wdth} `;
+    box.style.width = widthHeightValue;
+}
+
+function updateHeightValue(){
+
+    const Hght = heightInput.value + 'px';
+
+    const heightValue = `${Hght}`;
+    box.style.height = heightValue;
+
+}
 
 function copyCSS(){
 
-    const cssValue = `Border radius: ${ box.style.borderRadius};`;
+    const cssValue = `Border-radius: ${ box.style.borderRadius};\n    width: ${box.style.width};\n    height: ${box.style.height};`;
     navigator.clipboard.writeText(cssValue)
 
     .then(() => alert('CSS cópiado!'))
